ed70b45f0651ca1448621ee9a82e31ca6767ef97
AdrianJonathanMB <adrianjonathan540@gmail.com>
2025-08-27 22:39:38 -0300

    feat: funções de navegação e experiências

---
+++ index.js
@@ -0,0 +1,36 @@
+const navigators = document.querySelectorAll("[data-nav]");
+const experience = document.querySelectorAll("[data-experience]");
+const experienceTitle = document.querySelectorAll("[data-experience-title]");
+const experienceContent = document.querySelector("[data-experience-content]")
+
+const width = window.innerWidth;
+let experienceState = 0
+
+const experienceText = [
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    "Molestiae possimus eum rerum dolores rem deserunt nam reiciendis saepe cum.",
+    "Et ea beatae nulla harum suscipit asperiores ab at veniam odio.",
+    "teste 4",
+]
+
+navigators.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        document.documentElement.scrollLeft = index * width
+    })
+})
+
+experience.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        experienceState = index;
+        alert(experienceState)
+        if (experienceTitle[index] === experienceState) {
+            experienceTitle[index].classList.add("red")
+        } else {
+            experienceTitle[index].classList.remove("red")
+        }
+
+
+
+        experienceContent.innerHTML = experienceText[index]
+    })
+})
